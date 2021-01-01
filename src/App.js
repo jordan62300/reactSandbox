56c8762445948caab53bab9ef9bb12e07d4be164
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-01 12:26:38 +0100

    app generée

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
+
+
+ let state = {
+    clients: [
+      
+      {  id: 1, nom:"Jordan"},
+      {  id: 2, nom:"Justine"},
+      {  id: 3, nom:"Jack"}
+      
+    ]
+  }
+
+  const title="Liste des clients"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h1>{title}</h1>
+     <ul>
+       <li>Jordan <button>X</button></li>
+       <li>Justine<button>X</button></li>
+       <li>Jack<button>X</button></li>
+     </ul>
+     <form>
+       <input type="text" placeholder="Ajouter un client"/>
+       <button>Ajouter</button>
+     </form>
+   </div>
   );
 }
 
