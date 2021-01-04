a376624d287a27d228a63c5095a668b0b06ba8f9
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-04 22:03:51 +0100

    ajout des composants Themechoice & ToolBar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,37 @@ import ReactDOM from 'react-dom';
 import Client from './Client'
 import ClientForm from './ClientForm'
 import Counter from './Counter'
+import "./styles.css";
 
+// Composant en fin de chaine
+// Il reçoit dans ses props le thème et la fonction qui permet de le changer
+function ThemeChoice(props) {
+  const handleChange = event => {
+    const value = event.currentTarget.value;
+    props.updateTheme(value);
+  };
 
+  return (
+    <select name="theme" defaultValue={props.theme} onChange={handleChange}>
+      <option value="dark">Sombre</option>
+      <option value="light">Clair</option>
+    </select>
+  );
+}
+
+// Composant en deuxième ligne
+// Il reçoit dans ses props le thème et la fonction qui permet de le changer
+// Notons qu'en vrai il en a rien à foutre il s'en sert pas lui même
+// C'est uniquement pour pouvoir le passer au composant ThemeChoice ...
+function ToolBar(props) {
+  return (
+    <div>
+      <button>Zoomer</button>
+      <button>Dezoomer</button>
+      <ThemeChoice theme={props.theme} updateTheme={props.updateTheme} />
+    </div>
+  );
+}
 
 
 const App = () => {
@@ -19,7 +48,7 @@ const App = () => {
     {  id: 3, nom:"Jack"}
     
   ])
-
+  const [theme, setTheme] = useState("light");
 
  const addClient = (client) => {
     const updtatedClients = [...clients]
@@ -48,7 +77,12 @@ const App = () => {
      ))}
      </ul>
      <ClientForm onClientAdd={addClient}/>
+     <div className={theme}>
+      <ToolBar theme={theme} updateTheme={setTheme} />
+      <p>Theme utilisé : {theme}</p>
+    </div>
    </div>
+   
   );
 }
 
